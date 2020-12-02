08f860569d92687e6edd86694d87b790e9d3a75a
Zoey Mertes <zmertes@purdue.edu>
2020-12-01 21:54:06 -0500

    Add game timer and some other cleanup

--- match.c
+++ match.c
@@ -6,26 +6,37 @@
 #define false 0
 
 int play_game(int numcards) {
-    int c, i, clicks = 0, clicked = -1, remaining=numcards/2;
+    int c, i, clicked = -1;
+    // how many times the player has clicked on a card
+    int clicks = 0;
+    // how many matches are still in play
+    int remaining=numcards/2;
+    // game timer, in tenths of a second
+    int timer=0;
 	MEVENT event;
 	CARD* cards;
     cards = init_cards(numcards);
-    
+    halfdelay(1);
     // The game
 	while (remaining != 0) {
         clear();
         mvprintw(0, 0, "Matching Game!");
         mvprintw(1, 0, "Remaining matches: %d", remaining);
         mvprintw(2, 0, "Clicks taken: %d", clicks);
+        mvprintw(3, 0, "Timer: %02d:%02d", (timer/10)/60, (timer/10)%60);
         draw_cards(cards, numcards);
 		refresh();
+        
         c = getch();
-		if (c == KEY_MOUSE && getmouse(&event) == OK) {
+        if (c == ERR) {
+            //This is a timer call from halfdelay
+            timer += 1;
+        } else if (c == KEY_MOUSE && getmouse(&event) == OK) {
 			//Mouse clicked somewhere
             i = handle_potential_collision(cards, numcards, event.x, event.y);
             if (i != -1) {
-                clicks++;
                 //Card was clicked
+                clicks++;
                 if (clicked == -1) {
                     //This is the first card that was clicked, make note of it
                     clicked = i;
@@ -33,7 +44,8 @@ int play_game(int numcards) {
                     //Let them see what they clicked
                     draw_cards(cards, numcards);
                     refresh();
-                    sleep(1);
+                    napms(500);
+                    timer += 5;
                     //This is the second card that was clicked, see if we matched
                     if (cards[clicked].value == cards[i].value) {
                         // They matched!
@@ -52,6 +64,8 @@ int play_game(int numcards) {
 		}
 	}
 	
+	nocbreak(); //disable halfdelay
+	
 	// Post game
 	
     
@@ -65,13 +79,13 @@ int main() {
 	keypad(stdscr, TRUE);
 	cbreak(); //Disable line buffering
 	noecho();
-
-	printw("Card matching!");
-	refresh();
+	curs_set(0);
 	
 	mousemask(ALL_MOUSE_EVENTS, NULL);
-    
-    play_game(6);
+	
+	
+	
+    play_game(34);
 
 	endwin();
 	return 0;
